b03e8190a2fa825813b5d38016c036d8c9c10a36
kauanfiolo-04 <kauanfiolo@hotmail.com>
2023-06-15 17:42:24 -0300

    Voltando a fonte como veio ao mundo

--- sections/DesignSystem.tsx
+++ sections/DesignSystem.tsx
@@ -206,11 +206,11 @@ export interface Miscellaneous {
 
 export interface Font {
   /**
-   * @default 'Montserrat'
+   * @default 'Albert Sans'
    */
   fontFamily: string;
   /**
-   * @default @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap');
+   * @default @import url('https://fonts.googleapis.com/css2?family=Albert+Sans:wght@400;500;700&display=swap');
    * \@format css
    */
   styleInnerHtml: string;
