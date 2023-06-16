68031d5a1685ecf057644974123ee8215627df61
kauanfiolo-04 <kauanfiolo@hotmail.com>
2023-06-16 15:52:46 -0300

    Otimizando sections pesadas

--- sections/OfertasFogo.tsx
+++ sections/OfertasFogo.tsx
@@ -1,82 +1,65 @@
+import { useEffect, useId, useState, useRef } from 'preact/hooks'
+import { putSizeInUrl } from 'deco-sites/shp/FunctionsSHP/AddSizeInUrl.ts'
+import { DescontoPIX } from 'deco-sites/shp/FunctionsSHP/DescontoPix.ts'
 import ProdFogo from 'deco-sites/shp/components/ProductsSHP/ProdFogo.tsx'
-
-import type { LoaderReturnType } from '$live/types.ts'
+import { LoaderReturnType } from '$live/types.ts'
 import Slider from 'deco-sites/shp/components/ui/Slider.tsx'
 import SliderJS from 'deco-sites/shp/components/ui/SliderJS.tsx'
 import Icon from 'deco-sites/shp/components/ui/Icon.tsx'
 import Image from 'deco-sites/std/components/Image.tsx'
 import type { Product } from 'deco-sites/std/commerce/types.ts'
 
-import { DescontoPIX } from 'deco-sites/shp/FunctionsSHP/DescontoPix.ts'
-
-import { useEffect, useId, useState, useRef } from 'preact/hooks'
-
-import {putSizeInUrl} from 'deco-sites/shp/FunctionsSHP/AddSizeInUrl.ts'
-
 export interface Props {
   products: LoaderReturnType<Product[] | null>
-  /** @description data no formato AAAA-MM-DD*/
+  /** @description formato AAAA-MM-DD*/
   finalDaOferta: string
   interval: number
 }
 
 const calculateTimeRemaining = (startDate: Date, endDate: Date) => {
-  const startTime = startDate.getTime()
-  const endTime = endDate.getTime()
-  const difference = endTime - startTime
+  const difference = endDate.getTime() - startDate.getTime()
 
-  const days = Math.floor(difference / (1000 * 60 * 60 * 24))
-  const hours = Math.floor(
-    (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-  )
-  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
-  const seconds = Math.floor((difference % (1000 * 60)) / 1000)
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24)).toString().padStart(2, '0')
+  const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, '0')
+  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, '0')
+  const seconds = Math.floor((difference % (1000 * 60)) / 1000).toString().padStart(2, '0')
 
   return { days, hours, minutes, seconds }
 }
 
-const fireOffers = ({ products, finalDaOferta = '', interval = 0 }: Props) => {
-  //const [isMobile, setIsMobile] = useState(window.innerWidth <= 768)
-  const id = useId()+'-fogo'
+const FireOffers = ({ products, finalDaOferta = '', interval = 0 }: Props) => {
+  const id = useId() + '-fogo'
 
   const [days, setDays] = useState('00')
   const [hours, setHours] = useState('00')
   const [minutes, setMinutes] = useState('00')
   const [seconds, setSeconds] = useState('00')
 
-  const dataFinal: number[] = finalDaOferta.split('-').map((element, idx) =>idx === 1 ? parseInt(element) - 1 : parseInt(element))
-  const finalDate = dataFinal && new Date(dataFinal[0], dataFinal[1], dataFinal[2])
+  const finalDate = finalDaOferta ? new Date(finalDaOferta) : undefined
 
   const prev = useRef<HTMLDivElement>(null)
   const next = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    // const handleResize = () => {
-    //   setIsMobile(window.innerWidth <= 768)
-    // }
-
-    // handleResize()
-
-    // window.addEventListener('resize', handleResize)
-
     const timeOut = setInterval(() => {
-      const timeObj: boolean | Record<string, number> =
-        finalDate && calculateTimeRemaining(new Date(), finalDate)
-      if (typeof timeObj !== 'boolean') {
-        setDays(timeObj.days.toString().padStart(2, '0'))
-        setHours(timeObj.hours.toString().padStart(2, '0'))
-        setMinutes(timeObj.minutes.toString().padStart(2, '0'))
-        setSeconds(timeObj.seconds.toString().padStart(2, '0'))
+      const timeObj = finalDate && calculateTimeRemaining(new Date(), finalDate)
+      if (timeObj) {
+        const { days, hours, minutes, seconds } = timeObj
+        setDays(days)
+        setHours(hours)
+        setMinutes(minutes)
+        setSeconds(seconds)
       }
     }, 1000)
 
     return () => {
-      //window.removeEventListener('resize', handleResize)
       clearInterval(timeOut)
     }
   }, [])
 
-  if (!products || products.length === 0) return null
+  if (!products || products.length === 0) {
+    return null
+  }
 
   return (
     <div className='re1:w-[60vw] w-screen mx-auto'>
@@ -84,7 +67,11 @@ const fireOffers = ({ products, finalDaOferta = '', interval = 0 }: Props) => {
         <div className='flex gap-2 mx-auto items-center w-4/5'>
           <Image
             src='https://shopinfo.vteximg.com.br/arquivos/icone-ofertas-fogo.png'
-            height={30} width={30} preload fetchPriority='low' loading='lazy' 
+            height={30}
+            width={30}
+            preload
+            fetchPriority='low'
+            loading='lazy'
           />
           <span className='font-bold re1:text-2xl text-lg'>
             Suba de Nível no Arraiá Black da Shopinfo
@@ -93,7 +80,11 @@ const fireOffers = ({ products, finalDaOferta = '', interval = 0 }: Props) => {
         <div className='hidden re1:flex gap-1 ml-auto'>
           <button
             className='btn btn-circle min-w-[30px] min-h-[30px] max-h-[30px] max-w-[30px] bg-transparent hover:bg-transparent border border-[#dd1f26] hover:border-[#dd1f26]'
-            onClick={() => (prev.current && prev.current.firstChild instanceof HTMLButtonElement) && prev.current.firstChild.click()}
+            onClick={() =>
+              prev.current &&
+              prev.current.firstChild instanceof HTMLButtonElement &&
+              prev.current.firstChild.click()
+            }
           >
             <Icon
               class='text-[#dd1f26]'
@@ -104,7 +95,11 @@ const fireOffers = ({ products, finalDaOferta = '', interval = 0 }: Props) => {
           </button>
           <button
             className='btn btn-circle min-w-[30px] min-h-[30px] max-h-[30px] max-w-[30px] bg-transparent hover:bg-transparent border border-[#dd1f26] hover:border-[#dd1f26]'
-            onClick={() => (next.current && next.current.firstChild instanceof HTMLButtonElement) && next.current.firstChild.click()}
+            onClick={() =>
+              next.current &&
+              next.current.firstChild instanceof HTMLButtonElement &&
+              next.current.firstChild.click()
+            }
           >
             <Icon
               class='text-[#dd1f26]'
@@ -121,38 +116,48 @@ const fireOffers = ({ products, finalDaOferta = '', interval = 0 }: Props) => {
         className='container grid grid-cols-[20px_1fr_20px] re1:grid-cols-[48px_1fr_48px] px-0 re1:px-5'
       >
         <Slider className='carousel carousel-center gap-6 col-span-full row-start-2 row-end-5 scrollbar-none'>
-          {products.map((slide, index) => (
-            <Slider.Item
-              index={index}
-              class='carousel-item w-fit h-fit first:pl-6 last:pr-6'
-            >
-              <ProdFogo
-                imgUrl={
-                  slide.image && slide.image[0] && slide.image[0].url ? 
-                  (putSizeInUrl(slide.image[0].url,[135,135]) || slide.image[0].url) : '#'
-                }
-                nome={slide.name}
-                preco10={slide.offers?.highPrice && parseFloat((slide.offers?.highPrice / 10).toFixed(2))}
-                precoPIX={slide.offers && DescontoPIX(slide.offers.highPrice, 15)}
-                discountFlag={15}
-                timeRemaining={[days, hours, minutes, seconds]}
-              />
-            </Slider.Item>
-          ))}
+          {products.map((slide, index) => {
+            const imgUrl =
+              slide.image && slide.image[0] && slide.image[0].url
+                ? putSizeInUrl(slide.image[0].url, [135, 135]) ||
+                  slide.image[0].url
+                : ''
+
+            return (
+              <Slider.Item
+                index={index}
+                class='carousel-item w-fit h-fit first:pl-6 last:pr-6'
+              >
+                <ProdFogo
+                  imgUrl={imgUrl}
+                  nome={slide.name}
+                  preco10={
+                    slide.offers?.highPrice &&
+                    parseFloat((slide.offers.highPrice / 10).toFixed(2))
+                  }
+                  precoPIX={
+                    slide.offers && DescontoPIX(slide.offers.highPrice, 15)
+                  }
+                  discountFlag={15}
+                  timeRemaining={[days, hours, minutes, seconds]}
+                />
+              </Slider.Item>
+            )
+          })}
         </Slider>
 
-        <div class='hidden' ref={prev}>
+        <div className='hidden' ref={prev}>
           <Slider.PrevButton />
         </div>
 
-        <div class='hidden' ref={next}>
+        <div className='hidden' ref={next}>
           <Slider.NextButton />
         </div>
 
-        <SliderJS rootId={id} infinite interval={interval && interval * 1e3} />
+        <SliderJS rootId={id} infinite interval={interval * 1000} />
       </div>
     </div>
   )
 }
 
-export default fireOffers
+export default FireOffers
--- sections/Vitrine.tsx
+++ sections/Vitrine.tsx
@@ -1,24 +1,22 @@
+// deno-lint-ignore-file no-window-prefix
+import { useEffect, useId, useState } from 'preact/hooks'
+import { putSizeInUrl } from 'deco-sites/shp/FunctionsSHP/AddSizeInUrl.ts'
+import { DescontoPIX } from 'deco-sites/shp/FunctionsSHP/DescontoPix.ts'
 import PC from 'deco-sites/shp/components/ProductsSHP/PC.tsx'
 import Prod from 'deco-sites/shp/components/ProductsSHP/Prod.tsx'
-
-import type { LoaderReturnType } from '$live/types.ts'
 import Slider from 'deco-sites/shp/components/ui/Slider.tsx'
 import SliderJS from 'deco-sites/shp/components/ui/SliderJS.tsx'
 import type { Product } from 'deco-sites/std/commerce/types.ts'
+import type { LoaderReturnType } from '$live/types.ts'
 
-import { DescontoPIX } from 'deco-sites/shp/FunctionsSHP/DescontoPix.ts'
-
-import { useEffect, useId, useState } from 'preact/hooks'
-
-import {putSizeInUrl} from 'deco-sites/shp/FunctionsSHP/AddSizeInUrl.ts'
-
-export interface vitrineProps {
+export interface VitrineProps {
   PcGamer: boolean
   produtos: LoaderReturnType<Product[] | null>
 }
 
-const Shelf = ({ PcGamer, produtos }: vitrineProps) => {
+const Shelf = ({ PcGamer, produtos }: VitrineProps) => {
   const [isMobile, setIsMobile] = useState(false)
+  const id = useId() + '-vitrine'
 
   useEffect(() => {
     const handleResize = () => {
@@ -27,19 +25,79 @@ const Shelf = ({ PcGamer, produtos }: vitrineProps) => {
 
     handleResize()
 
-    // deno-lint-ignore no-window-prefix
     window.addEventListener('resize', handleResize)
 
     return () => {
-      // deno-lint-ignore no-window-prefix
       window.removeEventListener('resize', handleResize)
     }
   }, [])
 
-  const id = useId()+'-vitrine'
-
   if (!produtos || produtos.length === 0) {
     return null
+  }
+
+  const renderProduct = (element: Product, index: number) => {
+    const pecas: string[] = [
+      'Memória',
+      'SSD',
+      'HD',
+      'Processador',
+      'Placa de vídeo',
+    ]
+    const pecaArray = element.isVariantOf?.additionalProperty
+    const pecasObj: Record<string, string | undefined> = {}
+
+    pecaArray?.forEach((pecaObj) => {
+      if (pecas.includes(String(pecaObj.name))) {
+        if (pecaObj.name === 'HD' || pecaObj.name === 'SSD') {
+          pecaObj.value?.includes(pecaObj.name)
+            ? (pecasObj['armazenamento'] = pecaObj.value)
+            : (pecasObj['armazenamento'] = `${pecaObj.name}: ${pecaObj.value}`)
+        } else {
+          pecasObj[String(pecaObj.name)] = pecaObj.value
+        }
+      }
+    })
+
+    const commonProps = {
+      imgUrl:
+        element.image && element.image[0] && element.image[0].url
+          ? putSizeInUrl(element.image[0].url, [135, 135]) ||
+            element.image[0].url
+          : '#',
+      nome: element.name,
+      preco10:
+        element.offers?.highPrice &&
+        parseFloat((element.offers?.highPrice / 10).toFixed(2)),
+      precoPIX: element.offers && DescontoPIX(element.offers.highPrice, 15),
+      discountFlag: 15,
+    }
+
+    if (PcGamer) {
+      return (
+        <Slider.Item
+          index={index}
+          class='carousel-item w-fit h-fit first:pl-6 last:pr-6'
+        >
+          <PC
+            {...commonProps}
+            placa={pecasObj['Placa de vídeo']}
+            processador={pecasObj.Processador}
+            memoria={pecasObj.Memória}
+            armazenamento={pecasObj.armazenamento}
+          />
+        </Slider.Item>
+      )
+    } else {
+      return (
+        <Slider.Item
+          index={index}
+          class='carousel-item w-fit h-fit first:pl-6 last:pr-6'
+        >
+          <Prod {...commonProps} />
+        </Slider.Item>
+      )
+    }
   }
 
   return (
@@ -47,136 +105,19 @@ const Shelf = ({ PcGamer, produtos }: vitrineProps) => {
       {isMobile ? (
         <div className='container grid grid-cols-[48px_1fr_48px] px-0' id={id}>
           <Slider class='carousel carousel-center gap-6 col-span-full row-start-2 row-end-5 scrollbar-none'>
-            {PcGamer
-              ? produtos.map((element, index) => {
-                  const pecas: string[] = ['Memória','SSD','HD','Processador','Placa de vídeo']
-                  const pecaArray = element.isVariantOf?.additionalProperty
-                  const pecasObj: Record<string, string | undefined> = {}
-                  pecaArray?.forEach((pecaObj) => {
-                    if (pecas.includes(String(pecaObj.name))) {
-                      if(pecaObj.name === 'HD' || pecaObj.name === 'SSD'){
-                        pecaObj.value?.includes(pecaObj.name) 
-                        ? (pecasObj['armazenamento'] = pecaObj.value) : (pecasObj['armazenamento'] = pecaObj.name + ': ' + pecaObj.value)
-                      }else{
-                        pecasObj[String(pecaObj.name)] = pecaObj.value
-                      }
-                    }
-                  })
-
-                  return (
-                    <Slider.Item
-                      index={index}
-                      class='carousel-item w-fit h-fit first:pl-6 last:pr-6 '
-                    >
-                      <PC
-                        imgUrl={
-                          element.image && element.image[0] && element.image[0].url ? 
-                          (putSizeInUrl(element.image[0].url,[135,135]) || element.image[0].url) : '#'
-                        }
-                        nome={element.name}
-                        placa={pecasObj['Placa de vídeo']}
-                        processador={pecasObj.Processador}
-                        memoria={pecasObj.Memória}
-                        armazenamento={pecasObj.armazenamento}
-                        preco10={
-                          element.offers?.highPrice &&
-                          parseFloat(
-                            (element.offers?.highPrice / 10).toFixed(2)
-                          )
-                        }
-                        precoPIX={
-                          element.offers &&
-                          DescontoPIX(element.offers.highPrice, 15)
-                        }
-                        discountFlag={15}
-                      />
-                    </Slider.Item>
-                  )
-                })
-              : produtos.map((element, index) => (
-                  <Slider.Item
-                    index={index}
-                    class='carousel-item w-fit h-fit first:pl-6 last:pr-6'
-                  >
-                    <Prod
-                      imgUrl={
-                        element.image && element.image[0] && element.image[0].url ? 
-                        (putSizeInUrl(element.image[0].url,[135,135]) || element.image[0].url) : '#'
-                      }
-                      nome={element.name}
-                      preco10={
-                        element.offers?.highPrice &&
-                        parseFloat((element.offers?.highPrice / 10).toFixed(2))
-                      }
-                      precoPIX={
-                        element.offers &&
-                        DescontoPIX(element.offers.highPrice, 15)
-                      }
-                      discountFlag={15}
-                    />
-                  </Slider.Item>
-                ))}
+            {produtos.map(renderProduct)}
           </Slider>
           <SliderJS rootId={id} infinite />
         </div>
       ) : (
-        <div className={`grid ${(produtos.length%3===0 && produtos.length===6) ? 'grid-cols-3':'grid-cols-4'} gap-x-3 gap-y-3 w-fit mx-auto`}>
-          {PcGamer
-            ? produtos.map((element) => {
-                const pecas: string[] = ['Memória','SSD','HD','Processador','Placa de vídeo']
-                const pecaArray = element.isVariantOf?.additionalProperty
-                const pecasObj: Record<string, string | undefined> = {}
-                pecaArray?.forEach((pecaObj) => {
-                  if (pecas.includes(String(pecaObj.name))) {
-                    if(pecaObj.name === 'HD' || pecaObj.name === 'SSD'){
-                      pecaObj.value?.includes(pecaObj.name)
-                      ? (pecasObj['armazenamento'] = pecaObj.value) : (pecasObj['armazenamento'] = pecaObj.name + ': ' + pecaObj.value)
-                    }else{
-                      pecasObj[String(pecaObj.name)] = pecaObj.value
-                    }
-                  }
-                })
-
-                return (
-                  <PC
-                    imgUrl={
-                      element.image && element.image[0] && element.image[0].url ? 
-                      (putSizeInUrl(element.image[0].url,[135,135]) || element.image[0].url) : '#'
-                    }
-                    nome={element.name}
-                    placa={pecasObj['Placa de vídeo']}
-                    processador={pecasObj.Processador}
-                    memoria={pecasObj.Memória}
-                    armazenamento={pecasObj.armazenamento}
-                    preco10={
-                      element.offers?.highPrice &&
-                      parseFloat((element.offers?.highPrice / 10).toFixed(2))
-                    }
-                    precoPIX={
-                      element.offers &&
-                      DescontoPIX(element.offers.highPrice, 15)
-                    }
-                    discountFlag={15}
-                  />
-                )
-              })
-            : produtos.map((element) => (
-                <Prod
-                  imgUrl={
-                    element.image && element.image[0] && element.image[0].url ? 
-                    (putSizeInUrl(element.image[0].url,[135,135]) || element.image[0].url) : '#'
-                  }
-                  nome={element.name}
-                  preco10={
-                    element.offers?.highPrice &&
-                    parseFloat((element.offers?.highPrice / 10).toFixed(2))
-                  }
-                  precoPIX={
-                    element.offers && DescontoPIX(element.offers.highPrice, 15)
-                  }
-                  discountFlag={15}
-                />
-              ))}
+        <div
+          className={`grid ${
+            produtos.length % 3 === 0 && produtos.length === 6
+              ? 'grid-cols-3'
+              : 'grid-cols-4'
+          } gap-x-3 gap-y-3 w-fit mx-auto`}
+        >
+          {produtos.map(renderProduct)}
         </div>
       )}
     </div>
