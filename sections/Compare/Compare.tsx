5fb31314901efd4c67b8f57d78b398449d580eb9
kauanfiolo-04 <kauanfiolo@hotmail.com>
2023-11-13 10:59:57 -0300

    Compare Arrumado

--- sections/Compare/Compare.tsx
+++ sections/Compare/Compare.tsx
@@ -1,6 +1,7 @@
-import CompareContextProvider, { useCompareContext, CompareContextType, PcContextProps } from 'deco-sites/shp/contexts/Compare/CompareContext.tsx'
+import { useCompareContext, CompareContextType, PcContextProps } from 'deco-sites/shp/contexts/Compare/CompareContext.tsx'
 import Image from 'deco-sites/std/packs/image/components/Image.tsx'
 import CompareModal from 'deco-sites/shp/sections/Compare/CompareModal.tsx'
+import {useEffect} from 'preact/hooks'
 
 interface Props{
   PCs:PcContextProps[]
@@ -35,8 +36,9 @@ const PCCard=({PC}:{PC:PcContextProps})=>{
 }
 
 const Compare=({PCs}:Props)=>{
+  // useEffect(()=>console.log(PCs),[PCs])
+
   return (
-    <CompareContextProvider>
     <div className='fixed bottom-0 left-0 w-full re1:h-[100px] bg-black flex flex-col re1:flex-row p-[15px] re1:py-[15px] re1:px-0 z-[5]'>
       <div className='flex flex-col gap-2 re1:gap-0 re1:flex-row w-[90%] justify-around mb-2 re1:mb-0'>
         {PCs[0] ? <PCCard PC={PCs[0]}/> : <Vazio />}
@@ -46,7 +48,6 @@ const Compare=({PCs}:Props)=>{
       </div>
       <CompareModal PCs={PCs}/>
     </div>
-    </CompareContextProvider>
   )
 }
 
