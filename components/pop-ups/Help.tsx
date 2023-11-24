be76f297d19364539697aff30ba2c7abef7e0517
kauanfiolo-04 <kauanfiolo@hotmail.com>
2023-11-24 16:53:58 -0300

    subindo helperButton

--- components/pop-ups/Help.tsx
+++ components/pop-ups/Help.tsx
@@ -1,10 +1,34 @@
 import { useState, useEffect } from 'preact/hooks'
 
-const Help = () =>{
+export interface Props{
+  links:Array<{
+    text:string
+    icon:{type:'image', src:string} | {type:'svg', code:string}
+    href:string
+  }>
+}
+
+const Help = ({links}:Props) =>{
+  const [openLinks, setOpenLinks]=useState(false)
+
   return (
-    <>
-      Help
-    </>
+    <div className='flex flex-col gap-2 justify-end opacity-80 items-end'>
+      <div className={`${openLinks ? 'max-h-[400px] visible' : 'max-h-0 invisible'} overflow-hidden transition-[max-height] duration-300 ease-linear bg-white p-4 rounded-2xl`}>
+        {links.map(link=>{
+          return link.icon.type==='svg' ? (
+            <a href={link.href} className='flex gap-[10px] py-1 items-center justify-start border-y border-y-base-content first:border-t-transparent last:border-b-transparent' dangerouslySetInnerHTML={{__html:`${link.icon.code}<p>${link.text}</p>`}} />
+          ) : (
+            <a href={link.href} className='flex gap-[10px] py-1 items-center justify-start border-y border-y-base-content first:border-t-transparent last:border-b-transparent'>
+              <img src={link.icon.src}/>
+              <p>{link.text}</p>
+            </a>
+          )
+        })}
+      </div>
+      <button className={`btn btn-circle !bg-white text-black font-bold ${openLinks ? '' : '!w-fit px-2'}`} onClick={()=>setOpenLinks(!openLinks)}>
+        {openLinks ? '✕' : 'Ajuda?'}
+      </button>
+    </div>
   )
 }
 
--- sections/Pop-ups/HelperButtons.tsx
+++ sections/Pop-ups/HelperButtons.tsx
@@ -1,17 +1,29 @@
 // deno-lint-ignore-file no-window-prefix
 import { useState, useEffect } from 'preact/hooks'
 import ScrollToTop from 'deco-sites/shp/components/pop-ups/ScrollToTop.tsx'
+import Help from 'deco-sites/shp/components/pop-ups/Help.tsx'
 
-const HelperButtons=()=>{
+export interface Props{
+  links:Array<{
+    text:string
+    icon:{type:'image', src:string} | {type:'svg', code:string}
+    href:string
+  }>
+}
+
+const HelperButtons=({links}:Props)=>{
 
   const [showToTop, setShowToTop]=useState(false)
+  const [isPDP, setIsPDP]=useState(false)
 
   const handleScroll=()=>{
     setShowToTop(window.scrollY > window.innerHeight-200)
-  }
+  } 
 
   useEffect(()=>{
     if(typeof window !== 'undefined'){
+      window.location.pathname.split('/').pop() === 'p' && setIsPDP(true)
+
       window.addEventListener('scroll' ,handleScroll)
     }
 
@@ -21,7 +33,8 @@ const HelperButtons=()=>{
   },[])
 
   return (
-    <div className='fixed right-[20px] bottom-28'>
+    <div className={`fixed right-[20px] flex flex-col gap-3 items-end  ${isPDP ? 'bottom-40' : 'bottom-28'}`}>
+      <Help links={links}/>
       <div className={showToTop ? '' : 'hidden'}>
         <ScrollToTop />
       </div>
