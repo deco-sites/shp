87761e3be6c4ca7975376a14d114f6500af5fb12
kauanfiolo-04 <kauanfiolo@hotmail.com>
2023-12-11 15:58:11 -0300

    Arrumando compre junto

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,85 +2,86 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/_app.tsx";
-import * as $$0 from "./islands/AcessoriosIsland.tsx";
-import * as $$1 from "./islands/AddToCartButton/vtex.tsx";
-import * as $$2 from "./islands/BannerItem.tsx";
-import * as $$3 from "./islands/BuyBarIsland.tsx";
-import * as $$4 from "./islands/CampanhaIsland.tsx";
-import * as $$5 from "./islands/CompareContextIsland.tsx";
-import * as $$6 from "./islands/CompareIsland.tsx";
-import * as $$7 from "./islands/CompreJuntoIsland.tsx";
-import * as $$8 from "./islands/DescriptionIsland.tsx";
-import * as $$9 from "./islands/DetailsIsland.tsx";
-import * as $$10 from "./islands/GameContextIsland.tsx";
-import * as $$11 from "./islands/GamesIsland.tsx";
-import * as $$12 from "./islands/HeaderIsland.tsx";
-import * as $$13 from "./islands/InstitucionalIsland.tsx";
-import * as $$14 from "./islands/OfertasFogoIsland.tsx";
-import * as $$15 from "./islands/PCNivelGamer.tsx";
-import * as $$16 from "./islands/PagDepartamentoIsland.tsx";
-import * as $$17 from "./islands/Pop-ups/HelperButtonsIsland.tsx";
-import * as $$18 from "./islands/ProductImageZoom.tsx";
-import * as $$19 from "./islands/RecommendedProds.tsx";
-import * as $$20 from "./islands/SearchControls.tsx";
-import * as $$21 from "./islands/SearchIsland.tsx";
-import * as $$22 from "./islands/SearchMenuBarIsland.tsx";
-import * as $$23 from "./islands/SearchSubIsland.tsx";
-import * as $$24 from "./islands/SelectGamesIsland.tsx";
-import * as $$25 from "./islands/ShelfIsland.tsx";
-import * as $$26 from "./islands/ShippingSimulation.tsx";
-import * as $$27 from "./islands/SliderJS.tsx";
-import * as $$28 from "./islands/SpecificationIsland.tsx";
-import * as $$29 from "./islands/TesteIsland.tsx";
-import * as $$30 from "./islands/TesteMaylonIsland.tsx";
-import * as $$31 from "./islands/TrustvoxIsland.tsx";
-import * as $$32 from "./islands/VitrineIsland.tsx";
-import * as $$33 from "./islands/WishlistButton.tsx";
-import * as $$34 from "./islands/WishlistTesteIsland.tsx";
+import * as $_app from "./routes/_app.tsx";
+import * as $AcessoriosIsland from "./islands/AcessoriosIsland.tsx";
+import * as $AddToCartButton_vtex from "./islands/AddToCartButton/vtex.tsx";
+import * as $BannerItem from "./islands/BannerItem.tsx";
+import * as $BuyBarIsland from "./islands/BuyBarIsland.tsx";
+import * as $CampanhaIsland from "./islands/CampanhaIsland.tsx";
+import * as $CompareContextIsland from "./islands/CompareContextIsland.tsx";
+import * as $CompareIsland from "./islands/CompareIsland.tsx";
+import * as $CompreJuntoIsland from "./islands/CompreJuntoIsland.tsx";
+import * as $DescriptionIsland from "./islands/DescriptionIsland.tsx";
+import * as $DetailsIsland from "./islands/DetailsIsland.tsx";
+import * as $GameContextIsland from "./islands/GameContextIsland.tsx";
+import * as $GamesIsland from "./islands/GamesIsland.tsx";
+import * as $HeaderIsland from "./islands/HeaderIsland.tsx";
+import * as $InstitucionalIsland from "./islands/InstitucionalIsland.tsx";
+import * as $OfertasFogoIsland from "./islands/OfertasFogoIsland.tsx";
+import * as $PCNivelGamer from "./islands/PCNivelGamer.tsx";
+import * as $PagDepartamentoIsland from "./islands/PagDepartamentoIsland.tsx";
+import * as $Pop_ups_HelperButtonsIsland from "./islands/Pop-ups/HelperButtonsIsland.tsx";
+import * as $ProductImageZoom from "./islands/ProductImageZoom.tsx";
+import * as $RecommendedProds from "./islands/RecommendedProds.tsx";
+import * as $SearchControls from "./islands/SearchControls.tsx";
+import * as $SearchIsland from "./islands/SearchIsland.tsx";
+import * as $SearchMenuBarIsland from "./islands/SearchMenuBarIsland.tsx";
+import * as $SearchSubIsland from "./islands/SearchSubIsland.tsx";
+import * as $SelectGamesIsland from "./islands/SelectGamesIsland.tsx";
+import * as $ShelfIsland from "./islands/ShelfIsland.tsx";
+import * as $ShippingSimulation from "./islands/ShippingSimulation.tsx";
+import * as $SliderJS from "./islands/SliderJS.tsx";
+import * as $SpecificationIsland from "./islands/SpecificationIsland.tsx";
+import * as $TesteIsland from "./islands/TesteIsland.tsx";
+import * as $TesteMaylonIsland from "./islands/TesteMaylonIsland.tsx";
+import * as $TrustvoxIsland from "./islands/TrustvoxIsland.tsx";
+import * as $VitrineIsland from "./islands/VitrineIsland.tsx";
+import * as $WishlistButton from "./islands/WishlistButton.tsx";
+import * as $WishlistTesteIsland from "./islands/WishlistTesteIsland.tsx";
+import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
-    "./routes/_app.tsx": $0,
+    "./routes/_app.tsx": $_app,
   },
   islands: {
-    "./islands/AcessoriosIsland.tsx": $$0,
-    "./islands/AddToCartButton/vtex.tsx": $$1,
-    "./islands/BannerItem.tsx": $$2,
-    "./islands/BuyBarIsland.tsx": $$3,
-    "./islands/CampanhaIsland.tsx": $$4,
-    "./islands/CompareContextIsland.tsx": $$5,
-    "./islands/CompareIsland.tsx": $$6,
-    "./islands/CompreJuntoIsland.tsx": $$7,
-    "./islands/DescriptionIsland.tsx": $$8,
-    "./islands/DetailsIsland.tsx": $$9,
-    "./islands/GameContextIsland.tsx": $$10,
-    "./islands/GamesIsland.tsx": $$11,
-    "./islands/HeaderIsland.tsx": $$12,
-    "./islands/InstitucionalIsland.tsx": $$13,
-    "./islands/OfertasFogoIsland.tsx": $$14,
-    "./islands/PCNivelGamer.tsx": $$15,
-    "./islands/PagDepartamentoIsland.tsx": $$16,
-    "./islands/Pop-ups/HelperButtonsIsland.tsx": $$17,
-    "./islands/ProductImageZoom.tsx": $$18,
-    "./islands/RecommendedProds.tsx": $$19,
-    "./islands/SearchControls.tsx": $$20,
-    "./islands/SearchIsland.tsx": $$21,
-    "./islands/SearchMenuBarIsland.tsx": $$22,
-    "./islands/SearchSubIsland.tsx": $$23,
-    "./islands/SelectGamesIsland.tsx": $$24,
-    "./islands/ShelfIsland.tsx": $$25,
-    "./islands/ShippingSimulation.tsx": $$26,
-    "./islands/SliderJS.tsx": $$27,
-    "./islands/SpecificationIsland.tsx": $$28,
-    "./islands/TesteIsland.tsx": $$29,
-    "./islands/TesteMaylonIsland.tsx": $$30,
-    "./islands/TrustvoxIsland.tsx": $$31,
-    "./islands/VitrineIsland.tsx": $$32,
-    "./islands/WishlistButton.tsx": $$33,
-    "./islands/WishlistTesteIsland.tsx": $$34,
+    "./islands/AcessoriosIsland.tsx": $AcessoriosIsland,
+    "./islands/AddToCartButton/vtex.tsx": $AddToCartButton_vtex,
+    "./islands/BannerItem.tsx": $BannerItem,
+    "./islands/BuyBarIsland.tsx": $BuyBarIsland,
+    "./islands/CampanhaIsland.tsx": $CampanhaIsland,
+    "./islands/CompareContextIsland.tsx": $CompareContextIsland,
+    "./islands/CompareIsland.tsx": $CompareIsland,
+    "./islands/CompreJuntoIsland.tsx": $CompreJuntoIsland,
+    "./islands/DescriptionIsland.tsx": $DescriptionIsland,
+    "./islands/DetailsIsland.tsx": $DetailsIsland,
+    "./islands/GameContextIsland.tsx": $GameContextIsland,
+    "./islands/GamesIsland.tsx": $GamesIsland,
+    "./islands/HeaderIsland.tsx": $HeaderIsland,
+    "./islands/InstitucionalIsland.tsx": $InstitucionalIsland,
+    "./islands/OfertasFogoIsland.tsx": $OfertasFogoIsland,
+    "./islands/PCNivelGamer.tsx": $PCNivelGamer,
+    "./islands/PagDepartamentoIsland.tsx": $PagDepartamentoIsland,
+    "./islands/Pop-ups/HelperButtonsIsland.tsx": $Pop_ups_HelperButtonsIsland,
+    "./islands/ProductImageZoom.tsx": $ProductImageZoom,
+    "./islands/RecommendedProds.tsx": $RecommendedProds,
+    "./islands/SearchControls.tsx": $SearchControls,
+    "./islands/SearchIsland.tsx": $SearchIsland,
+    "./islands/SearchMenuBarIsland.tsx": $SearchMenuBarIsland,
+    "./islands/SearchSubIsland.tsx": $SearchSubIsland,
+    "./islands/SelectGamesIsland.tsx": $SelectGamesIsland,
+    "./islands/ShelfIsland.tsx": $ShelfIsland,
+    "./islands/ShippingSimulation.tsx": $ShippingSimulation,
+    "./islands/SliderJS.tsx": $SliderJS,
+    "./islands/SpecificationIsland.tsx": $SpecificationIsland,
+    "./islands/TesteIsland.tsx": $TesteIsland,
+    "./islands/TesteMaylonIsland.tsx": $TesteMaylonIsland,
+    "./islands/TrustvoxIsland.tsx": $TrustvoxIsland,
+    "./islands/VitrineIsland.tsx": $VitrineIsland,
+    "./islands/WishlistButton.tsx": $WishlistButton,
+    "./islands/WishlistTesteIsland.tsx": $WishlistTesteIsland,
   },
   baseUrl: import.meta.url,
-};
+} satisfies Manifest;
 
 export default manifest;
