3ca101f224454ddefb047c67c33066cd72806779
kauanfiolo-04 <kauanfiolo@hotmail.com>
2023-12-28 17:54:58 -0300

    tentando

--- sections/LPs/ParaSuaEmpresa.tsx
+++ sections/LPs/ParaSuaEmpresa.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'preact/hooks'
 import Image from 'deco-sites/std/packs/image/components/Image.tsx'
 import Icon from 'deco-sites/shp/components/ui/Icon.tsx'
+import Form from 'deco-sites/shp/sections/LPs/ParaSuaEmpresa.tsx'
 
 const LpB2B=()=>{
   return (
@@ -8,6 +9,7 @@ const LpB2B=()=>{
       <section className='flex justify-end items-center h-[90vh] bg-center bg-cover bg-[url(https://shopinfo.vteximg.com.br/arquivos/LP-mobile-B2B-bg-1.jpg)] re1:bg-[url(https://shopinfo.vteximg.com.br/arquivos/LP-desktop-B2B-bg-1.jpg)]'>
         <div className='flex flex-col gap-2 items-center'>
           <p className='text-white text-xl text-center font-bold'>Sua empresa equipada<br/>com os melhores produtos<br/>de informática do mercado!</p>
+          {/* <Form /> */}
         </div>
       </section>
 
@@ -46,9 +48,65 @@ const LpB2B=()=>{
           <Image src='https://shopinfo.vteximg.com.br/arquivos/lp-icone-b2b-Pedido-Personalizado.png' width={175} height={153}/>
         </div>
       </section>
-      <section className='h-[90vh] bg-center bg-cover bg-[url(https://shopinfo.vteximg.com.br/arquivos/icone-lp-b2b-gabinetepc.png)]'>
-        <div className='flex flex-col'>
-          <h1>Compre e<br/><b className='font-bold text-3xl'>Confie</b></h1>
+
+      <section className='h-screen bg-center bg-[url(https://shopinfo.vteximg.com.br/arquivos/LP-desktop-B2B-bg-4.jpg)]'>
+        <div className='flex flex-col justify-center gap-8 h-[70%] text-neutral'>
+          <h1 className='mx-auto text-2xl'>Compre e<br/><b className='font-bold text-4xl'>Confie</b></h1>
+          <div className='pl-12 flex flex-col re1:flex-row w-full re1:w-[70%]'>
+            <div className='flex flex-col items-start justify-center w-[50%]'>
+              <label className='flex gap-2 justify-center items-center h-[100px]'>
+                <Image src='https://shopinfo.vteximg.com.br/arquivos/icone-lp-b2b-facilidadenacompra.png' width={70} height={70}/>
+                <div>
+                  <h1 className='text-xl font-bold'>FACILIDADE NA COMPRA</h1>
+                  <span className='text-base'>Basta navegar entre as diversas páginas de produtos (categorias, novidades, ofertas, destaques) e clicar sobre o icone Comprar.</span>
+                </div>
+              </label>
+              <label className='flex gap-2 justify-center items-center h-[100px]'>
+                <Image src='https://shopinfo.vteximg.com.br/arquivos/icone-lp-b2b-autenticidade.png' width={70} height={70}/>
+                <div>
+                  <h1 className='text-xl font-bold'>AUTENTICIDADE</h1>
+                  <span className='text-base'>Trabalhamos somente com produtos Originais e entregamos todos os nossos produtos com Nota Fiscal.</span>
+                </div>
+              </label>
+              <label className='flex gap-2 justify-center items-center h-[100px]'>
+                <Image src='https://shopinfo.vteximg.com.br/arquivos/icone-lp-b2b-embalagem.png' width={70} height={70}/>
+                <div>
+                  <h1 className='text-xl font-bold'>EMBALAGEM SEGURA</h1>
+                  <span className='text-base'>Os produtos são muito bem embalados em caixas resistentes. Realizamos entrega em todo o território nacional.</span>
+                </div>
+              </label>
+            </div>
+            <div className='flex flex-col items-start justify-center w-[50%]'>
+              <label className='flex gap-2 justify-center items-center h-[100px]'>
+                <Image src='https://shopinfo.vteximg.com.br/arquivos/icone-lp-b2b-melhordetudo.png' width={70} height={70}/>
+                <div>
+                  <h1 className='text-xl font-bold'>O MELHOR DE TUDO</h1>
+                  <span className='text-base'>Os PCs Neologic são cuidadosamente selecionados com os melhores componentes do mercado e oferecemos, inúmeras opções de configuração desde o Básico até o Premium.</span>
+                </div>
+              </label>
+              <label className='flex gap-2 justify-center items-center h-[100px]'>
+                <Image src='https://shopinfo.vteximg.com.br/arquivos/icone-lp-b2b-trabalho.png' width={70} height={70}/>
+                <div>
+                  <h1 className='text-xl font-bold'>DESIGN INDUSTRIAL E ALTA PERFORMANCE</h1>
+                  <span className='text-base'>Design discreto, desempenho máximo. Nossas workstations já vem montados #WorkReady.</span>
+                </div>
+              </label>
+              <label className='flex gap-2 justify-center items-center h-[100px]'>
+                <Image src='https://shopinfo.vteximg.com.br/arquivos/icone-lp-b2b-seguran%C3%A7a.png' width={70} height={70}/>
+                <div>
+                  <h1 className='text-xl font-bold'>SEGURANÇA E CONFIABILIDADE</h1>
+                  <span className='text-base'>Temos o compromisso com a Privacidade e a segurança dos, clientes, por isso o nosso site conta com Certificado de Navegação Segura.</span>
+                </div>
+              </label>
+            </div>
+          </div>
+          <Image className='absolute right-0 mt-8' src='https://shopinfo.vteximg.com.br/arquivos/icone-lp-b2b-gabinetepc.png' width={424} height={544}/>
+        </div>
+
+        <div className='flex gap-12 justify-center items-center h-[20%]'>
+          <Image src='https://shopinfo.vteximg.com.br/arquivos/lp-icone-b2b-nota-relclame-aqui-8,0.png' width={86} height={100} />
+          <Image src='https://shopinfo.vteximg.com.br/arquivos/icone-lp-reclame-aqui.png' width={650} height={254} />
+          <Image src='https://shopinfo.vteximg.com.br/arquivos/icone-lp-notagoogle.png' width={91} height={100} />
         </div>
       </section>
     </>
