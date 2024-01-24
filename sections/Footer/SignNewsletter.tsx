45b13d52cc527cdc9791f22361f07e0277acdb94
pedrohosantana <pedrohosantana@gmail.com>
2024-01-24 11:32:32 -0300

    Aplicando outra abordagem para o form
    Adicionando defer com outra abordagem para o formulario da RDStation

--- sections/Footer/SignNewsletter.tsx
+++ sections/Footer/SignNewsletter.tsx
@@ -1,3 +1,20 @@
-const SignNewsletter=()=><div className='re1:px-[30%]' dangerouslySetInnerHTML={{__html:`<div role="main" id="footer-057c7ba33caa73ecd1d1"></div><script type="text/javascript" defer src="https://d335luupugsy2.cloudfront.net/js/rdstation-forms/stable/rdstation-forms.min.js"></script><script type="text/javascript" defer> new RDStationForms('footer-057c7ba33caa73ecd1d1', 'null').createForm();</script>`}}/>
+const SignNewsletter = () => (
+  <div className='re1:px-[30%]' dangerouslySetInnerHTML={{__html: `
+    <div role="main" id="footer-057c7ba33caa73ecd1d1"></div>
+    <script type="text/javascript">
+      var firstScript = document.createElement('script');
+      firstScript.src = 'https://d335luupugsy2.cloudfront.net/js/rdstation-forms/stable/rdstation-forms.min.js';
+      firstScript.defer = true;
 
-export default SignNewsletter
+      firstScript.onload = function() {
+        var secondScript = document.createElement('script');
+        secondScript.text = "new RDStationForms('footer-057c7ba33caa73ecd1d1', 'null').createForm();";
+        document.body.appendChild(secondScript);
+      };
+
+      document.body.appendChild(firstScript);
+    </script>
+  `}}/>
+);
+
+export default SignNewsletter;
