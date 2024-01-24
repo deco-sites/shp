08c66826e5ec2e5fe0de2002e2a7e181819db669
pedrohosantana <pedrohosantana@gmail.com>
2024-01-24 11:21:21 -0300

    Adicionando defer no form
    Adicionando defer no formulario de newsletter da RD Station

--- sections/Footer/SignNewsletter.tsx
+++ sections/Footer/SignNewsletter.tsx
@@ -1,3 +1,3 @@
-const SignNewsletter=()=><div className='re1:px-[30%]' dangerouslySetInnerHTML={{__html:`<div role="main" id="footer-057c7ba33caa73ecd1d1"></div><script type="text/javascript" src="https://d335luupugsy2.cloudfront.net/js/rdstation-forms/stable/rdstation-forms.min.js"></script><script type="text/javascript"> new RDStationForms('footer-057c7ba33caa73ecd1d1', 'null').createForm();</script>`}}/>
+const SignNewsletter=()=><div className='re1:px-[30%]' dangerouslySetInnerHTML={{__html:`<div role="main" id="footer-057c7ba33caa73ecd1d1"></div><script type="text/javascript" defer src="https://d335luupugsy2.cloudfront.net/js/rdstation-forms/stable/rdstation-forms.min.js"></script><script type="text/javascript" defer> new RDStationForms('footer-057c7ba33caa73ecd1d1', 'null').createForm();</script>`}}/>
 
 export default SignNewsletter
