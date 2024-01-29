169d5247b0ae06ddc882d7ba254fba364ab46989
pedrohosantana <pedrohosantana@gmail.com>
2024-01-29 10:49:09 -0300

    Removendo preload dos icones de categoria
    Removendo preload dos icones da barra de categorias

--- sections/PagCategEDepto/iconeNavegacional.tsx
+++ sections/PagCategEDepto/iconeNavegacional.tsx
@@ -12,7 +12,7 @@ const IconeNavegacional=({href, categoryName, imgUrl}:Props)=>(
     bg-cover bg-center bg-no-repeat hover:bg-primary transition ease-in-out duration-300 hover:re1:shadow-[0_0_20px_0] hover:re1:shadow-primary
     flex items-center justify-center'>
     <a href={href} className='flex flex-col w-full h-full text-center'>
-      <Image className='m-auto' src={imgUrl} alt={categoryName} width={70} height={70} fetchPriority='high' preload loading='eager' decoding='sync'/>
+      <Image className='m-auto' src={imgUrl} alt={categoryName} width={70} height={70} fetchPriority='high' loading='eager' decoding='sync'/>
       <span className='text-secondary text-[11px] re1:text-xs font-bold mb-[10px]'>{categoryName}</span>
     </a> 
   </li>
