de5b36ad54b0dcf0a26eea19fd639833dddd5f03
kauanfiolo-04 <kauanfiolo@hotmail.com>
2024-02-02 09:00:43 -0300

    Pf de certo

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,12 +1,7 @@
-/// <reference no-default-lib="true"/>
-/// <reference lib="dom" />
-/// <reference lib="deno.ns" />
-/// <reference lib="esnext" />
-
 import plugins from "deco-sites/std/plugins/mod.ts";
 import partytownPlugin from "partytown/mod.ts";
 
-import decoManifest from "./manifest.gen.ts";
+import manifest from "./manifest.gen.ts";
 import tailwind from "./tailwind.config.ts";
 
 import { defineConfig } from "$fresh/server.ts";
@@ -14,7 +9,7 @@ export default defineConfig({
   plugins: [
     ...plugins(
       {
-        manifest: decoManifest,
+        manifest,
         // deno-lint-ignore no-explicit-any
         tailwind: tailwind as any,
       },
--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+/// <reference no-default-lib="true"/>
+/// <reference lib="dom" />
+/// <reference lib="deno.ns" />
+/// <reference lib="esnext" />
+
 import { start } from "$fresh/server.ts";
 import config from "./fresh.config.ts";
 import manifest from "./fresh.gen.ts";
