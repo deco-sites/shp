2e46d1e0dd9bbd90ad123847fc225035bc202c77
kauanfiolo-04 <kauanfiolo@hotmail.com>
2024-02-02 09:06:02 -0300

    pf

--- main.ts
+++ main.ts
@@ -1,8 +1,3 @@
-/// <reference no-default-lib="true"/>
-/// <reference lib="dom" />
-/// <reference lib="deno.ns" />
-/// <reference lib="esnext" />
-
 import { start } from "$fresh/server.ts";
 import config from "./fresh.config.ts";
 import manifest from "./fresh.gen.ts";
