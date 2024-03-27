fafc250be479372a0ec880af962a0dbbdf46d134
kauanfiolo-04 <kauanfiolo@hotmail.com>
2024-03-27 17:43:11 -0300

    Começando alterações Home

--- sections/Benefits.tsx
+++ sections/Benefits.tsx
@@ -16,36 +16,36 @@ export const loader = (props: Props, _req: Request, ctx: AppContext & {descontoP
 
 const Benefits = ({descontoPix}:SectionProps<typeof loader>) => {
   return (
-    <div className="flex flex-row text-secondary items-center re1:justify-center mx-auto gap-6 re1:gap-16 re4:gap-24 m-3 w-11/12 h-24 overflow-x-auto scrollbar-none text-sm">
-        <label className="min-w-[150px] re1:min-w-0 text-center re1:text-left flex re1:flex-row flex-col items-center gap-3">
+    <div className="flex flex-row text-secondary items-center re1:justify-center mx-auto gap-6 re1:gap-16 re4:gap-24 m-3 w-11/12 h-24 overflow-x-scroll scrollbar-none text-sm">
+        <label className="min-w-[45%] re1:min-w-0 text-left flex items-center gap-3">
           <Image
             decoding='sync' loading='eager' fetchPriority='high'
             className="" width={49} height={49} src="https://shopinfo.vteximg.com.br/arquivos/icone-home-beneficios-pcmontado.png" 
           />
           <p><b>PC Montado</b> <br/> Pronto para Jogar</p>
         </label>
-        <label className="min-w-[150px] re1:min-w-0 text-center re1:text-left flex re1:flex-row flex-col items-center gap-3">
+        <label className="min-w-[45%] re1:min-w-0 text-left flex items-center gap-3">
           <Image
             decoding='sync' loading='eager' fetchPriority='high'
             className="" width={47} height={47} src="https://shopinfo.vteximg.com.br/arquivos/icone-home-beneficios-pix-.png" 
           />
           <p><b>{descontoPix}%</b> OFF<br/>no PIX</p>
         </label>
-        <label className="min-w-[150px] re1:min-w-0 text-center re1:text-left flex re1:flex-row flex-col items-center gap-3">
+        <label className="min-w-[45%] re1:min-w-0 text-left flex items-center gap-3">
           <Image
             decoding='sync' loading='eager' fetchPriority='high'
             className="" width={84} height={35} src="https://shopinfo.vteximg.com.br/arquivos/icone-home-beneficios-10x-.png" 
           />
           <p>Sem juros <br/> no cartão</p>
         </label>    
-        <label className="min-w-[150px] re1:min-w-0 text-center re1:text-left flex re1:flex-row flex-col items-center gap-3">
+        <label className="min-w-[45%] re1:min-w-0 text-left flex items-center gap-3">
           <Image
             decoding='sync' loading='eager' fetchPriority='high'
             className="" width={48} height={40} src="https://shopinfo.vteximg.com.br/arquivos/icone-home-beneficios-2cartoes-.png" 
           />
           <p>Parcele sua compra<br/> com<b> 2 cartões</b></p>
         </label>
-        <label className="min-w-[150px] re1:min-w-0 text-center re1:text-left flex re1:flex-row flex-col items-center gap-3">
+        <label className="min-w-[45%] re1:min-w-0 text-left flex items-center gap-3">
           <Image
             decoding='sync' loading='eager' fetchPriority='high'
             className="" width={62} height={35} src="https://shopinfo.vteximg.com.br/arquivos/icon-truck-frete.png" 
