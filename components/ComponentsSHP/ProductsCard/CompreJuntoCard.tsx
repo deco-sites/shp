195b6fabfd57b71bd09f4a077086003b9f01738b
kauanfiolo-04 <kauanfiolo@hotmail.com>
2024-04-02 11:58:29 -0300

    Acabei compre junto

--- components/ComponentsSHP/ProductsCard/CompreJuntoCard.tsx
+++ components/ComponentsSHP/ProductsCard/CompreJuntoCard.tsx
@@ -7,6 +7,8 @@ import { TimeRemaining } from 'deco-sites/shp/FunctionsSHP/useTimer.ts'
 import { DescontoPIX } from 'deco-sites/shp/FunctionsSHP/DescontoPix.ts'
 import { useCompareContext, CompareContextType, PcContextProps } from 'deco-sites/shp/contexts/Compare/CompareContext.tsx'
 import { useOffer } from 'deco-sites/fashion/sdk/useOffer.ts'
+import Icon from "deco-sites/shp/components/ui/Icon.tsx";
+import { useCart } from "apps/vtex/hooks/useCart.ts";
 
 interface comboObj{
   id:string
@@ -59,6 +61,8 @@ const CardProd=(props:CardProps)=>{
   const {days, hours, minutes, seconds} = props.timeRemaining || {days:'00', hours:'00', minutes:'00', seconds:'00'}
   const salePricePix=DescontoPIX(props.precoVista, props.pix)
   const diffPercent=Math.ceil(-1*(((100*salePricePix)/props.precoDe)-100))
+
+  const {addItems}=useCart()
 
   return (
     <a href={props.linkProd} className='flex flex-col re1:flex-row w-full h-[215px] re1:h-[245px] bg-[#171717] hover:shadow-[0_10px_25px_0] hover:shadow-[rgba(0,0,0,.85)]
@@ -164,16 +168,62 @@ const CardProd=(props:CardProps)=>{
         </div>
   
         <div className='flex flex-col items-center justify-around bg-[#353535] py-[10px] text-center'>
-          <div className='flex flex-col items-center justify-between'>
-            <Image src={props.combo?.image ?? ''} width={150} height={150}/>
-            <p>{props.combo?.name}</p>
-            <div className='w-full py-2 flex gap-3 justify-center bg-primary border-primary hover:border-primary hover:bg-primary rounded-lg'>
-              <Image src='https://shopinfo.vteximg.com.br/arquivos/vector-cart-buy-button.png' className='h-[18px] my-auto'
-                width={22} height={18} decoding='auto' fetchPriority='high' loading='eager'
-              />
-              <p className='font-bold text-base re1:text-lg text-secondary capitalize'>Compre e Ganhe</p>
-            </div>
-          </div>
+          {!props.combo ? (
+            <>
+              <div>
+                <span className='text-sm'>Restam:</span>
+                <div className='bg-[#00a74c] text-2xl font-bold rounded-lg w-fit p-[10px] mx-auto'>{props.quantidade}</div>
+                <span className='text-sm'>Itens Disponíveis</span>
+              </div>
+            
+              {props.timeRemaining && (
+                <div>  
+                  <span>Essa oferta<br/>termina em:</span>
+                  <div className='bg-primary rounded-lg flex justify-around p-1 items-center w-44'>
+                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#fff" height={25} width={25}  x="0px" y="0px" viewBox="0 0 31.802 31.802" enable-background="new 0 0 31.802 31.802;" ><g><path d="M29.484,9.939l-3.896-4.673l-2.703,2.255c-1.198-0.8-2.539-1.396-3.969-1.771V3.609h2.164V0h-8.868v3.609h2.475v1.74 C7.792,5.805,2.317,11.545,2.317,18.553c0,7.307,5.943,13.249,13.248,13.249c7.307,0,13.249-5.942,13.249-13.249 c0-2.396-0.649-4.64-1.767-6.581L29.484,9.939z M16.081,29.301v-3.109h-0.515v3.137c-2.809,0-5.361-1.09-7.282-2.857 c0.229-0.148,0.391-0.396,0.391-0.691c0-0.465-0.378-0.842-0.843-0.842c-0.276,0-0.51,0.143-0.664,0.349 c-1.481-1.845-2.373-4.181-2.375-6.726H7.47v-0.515H4.818c0.123-2.621,1.183-4.996,2.856-6.803 c0.155,0.184,0.378,0.309,0.638,0.309c0.465,0,0.842-0.377,0.842-0.843c0-0.236-0.099-0.448-0.255-0.601 c1.836-1.453,4.149-2.328,6.666-2.328v3.615h0.515V7.806c2.266,0.107,4.348,0.914,6.038,2.216 c-0.119,0.145-0.203,0.325-0.203,0.53c0,0.466,0.377,0.843,0.844,0.843c0.236,0,0.449-0.101,0.603-0.258 c1.729,1.818,2.828,4.236,2.953,6.908h-2.535v0.514h2.562c-0.002,2.106-0.619,4.066-1.668,5.729 c-0.144-0.114-0.316-0.192-0.514-0.192c-0.466,0-0.843,0.378-0.843,0.843c0,0.285,0.151,0.524,0.368,0.679 C21.813,27.762,19.11,29.157,16.081,29.301z"></path><path d="M16.234,10.708v4.16c1.786,0.315,3.153,1.817,3.276,3.662l4.089-0.669C23.163,14.051,20.079,11.035,16.234,10.708z"></path></g></svg>
+                    <span className='font-bold text-xl w-[80%]'>{`${days}D ${hours}:${minutes}:${seconds}`}</span>
+                  </div>
+                </div>)}
+            </>
+          ) : (
+            <>
+              <div className='bg-[#353535] p-2 re1:mr-[18%] re4:mr-[16%] absolute flex items-center justify-center border border-primary rounded-full'>
+                <Icon
+                  id="Plus"
+                  size={25}
+                  strokeWidth={3}
+                />
+              </div>
+              <div className='flex flex-col items-center justify-between'>
+                <Image src={props.combo?.image} width={120} height={120}/>
+                <p className='line-clamp-2 px-4'>{props.combo?.name}</p>
+                <p className='font-bold'>Total: <span className='text-success'>{(props.combo?.finalPrice + salePricePix).toLocaleString('pt-BR',{style:'currency', currency:'BRL'})}</span></p>
+                <button className='w-full re1:w-[80%] py-2 flex gap-3 justify-center bg-primary border-primary hover:border-primary hover:bg-primary rounded-lg'
+                  onClick={()=>{
+                    addItems({
+                      orderItems: [
+                        {
+                          id: props.prodId,
+                          seller: '1',
+                          quantity: 1,
+                        },
+                        {
+                          id: props.combo?.id ?? '',
+                          seller: '1',
+                          quantity: 1,
+                        },
+                      ],
+                    })
+                  }}
+                >
+                  <Image src='https://shopinfo.vteximg.com.br/arquivos/vector-cart-buy-button.png' className='h-[18px] my-auto'
+                    width={22} height={18} decoding='auto' fetchPriority='high' loading='eager'
+                  />
+                  <p className='font-bold text-base re1:text-lg text-secondary capitalize'>Compre Junto</p>
+                </button>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </a>)
@@ -190,6 +240,8 @@ const CardPC=({NLI, placaVideo, processador, memoria, armazenamento, tipoArm,...
     name:props.prodName, id:props.prodId, parcelas:props.parcelas, valorParcela:props.valorParcela, groupId:props.groupId,
     precoDe:props.precoDe, precoVista:props.precoVista, linkProd:props.linkProd, imgUrl:props.imgUrl, pix:props.pix
   }
+
+  const {addItems}=useCart()
 
   useEffect(()=>{
     if(!PCs.some((pc)=>pc.id===pcObj.id && pc.name===pcObj.name)){
@@ -411,16 +463,43 @@ const CardPC=({NLI, placaVideo, processador, memoria, armazenamento, tipoArm,...
               </div>)}
           </>
         ) : (
-          <div className='flex flex-col items-center justify-between'>
-            <Image src={props.combo?.image} width={150} height={150}/>
-            <p>{props.combo?.name}</p>
-            <div className='w-full py-2 flex gap-3 justify-center bg-primary border-primary hover:border-primary hover:bg-primary rounded-lg'>
-              <Image src='https://shopinfo.vteximg.com.br/arquivos/vector-cart-buy-button.png' className='h-[18px] my-auto'
-                width={22} height={18} decoding='auto' fetchPriority='high' loading='eager'
+          <>
+            <div className='bg-[#353535] p-2 re1:mr-[18%] re4:mr-[16%] absolute flex items-center justify-center border border-primary rounded-full'>
+              <Icon
+                id="Plus"
+                size={25}
+                strokeWidth={3}
               />
-              <p className='font-bold text-base re1:text-lg text-secondary capitalize'>Compre e Ganhe</p>
             </div>
-          </div>
+            <div className='flex flex-col items-center justify-between'>
+              <Image src={props.combo?.image} width={120} height={120}/>
+              <p className='line-clamp-2 px-4'>{props.combo?.name}</p>
+              <p className='font-bold'>Total: <span className='text-success'>{(props.combo?.finalPrice + salePricePix).toLocaleString('pt-BR',{style:'currency', currency:'BRL'})}</span></p>
+              <button className='w-full re1:w-[80%] py-2 flex gap-3 justify-center bg-primary border-primary hover:border-primary hover:bg-primary rounded-lg'
+                onClick={()=>{
+                  addItems({
+                    orderItems: [
+                        {
+                          id: props.prodId,
+                          seller: '1',
+                          quantity: 1,
+                        },
+                        {
+                          id: props.combo?.id ?? '',
+                          seller: '1',
+                          quantity: 1,
+                        },
+                      ],
+                  })
+                }}
+              >
+                <Image src='https://shopinfo.vteximg.com.br/arquivos/vector-cart-buy-button.png' className='h-[18px] my-auto'
+                  width={22} height={18} decoding='auto' fetchPriority='high' loading='eager'
+                />
+                <p className='font-bold text-base re1:text-lg text-secondary capitalize'>Compre Junto</p>
+              </button>
+            </div>
+          </>
         )}
       </div>
     </div>
