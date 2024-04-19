357131205452a0efb04d069d3af7623c38df99d6
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2024-04-19 11:57:45 -0300

    Remove sw

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -2,6 +2,17 @@ import { PageProps } from '$fresh/server.ts'
 import GlobalTags from 'deco-sites/fashion/components/GlobalTags.tsx'
 import DesignSystem from 'deco-sites/fashion/sections/DesignSystem.tsx'
 
+const sw = () =>
+  addEventListener("load", () => {
+    if (navigator && navigator.serviceWorker) {
+      navigator.serviceWorker.getRegistrations().then((registrations) => {
+        for (const registration of registrations) {
+          registration.unregister();
+        }
+      });
+    }
+  });
+
 function App(props: PageProps) {
   return (
     <>
@@ -14,6 +25,12 @@ function App(props: PageProps) {
 
       {/* Rest of Preact tree */}
       <props.Component />
+
+      {/* Include service worker */}
+      <script
+        type="module"
+        dangerouslySetInnerHTML={{ __html: `(${sw})();` }}
+      />
     </>
   )
 }
