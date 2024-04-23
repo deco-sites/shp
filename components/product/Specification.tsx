737a9c86c41a2753c0b82d2f091daac87f16fe06
Kauan Fiolo da Silva <kauanfiolo@hotmail.com>
2024-04-23 14:41:32 -0300

    Arrumando benefits

--- components/product/Specification.tsx
+++ components/product/Specification.tsx
@@ -4,6 +4,7 @@ import type { Product, ProductDetailsPage } from 'apps/commerce/types.ts'
 import Image from 'deco-sites/std/components/Image.tsx'
 import { sendEvent } from 'deco-sites/shp/sdk/analytics.tsx'
 import SpecPeca from "deco-sites/shp/components/product/SpecPeca.tsx";
+import { FalseLiteral } from "https://deno.land/x/ts_morph@21.0.1/mod.ts";
 
 export interface Props {
   page: ProductDetailsPage
@@ -16,109 +17,113 @@ const Specification=({page}:Props)=>{
   )
   const PCGamer = categoriesId?.some((item) => item === '10')
   const description:string=product.description!
-  const [openMenu,setOpenMenu]=useState(()=>{
-    if(!PCGamer){
-      const descElement=document.createElement('div')
-      descElement.innerHTML=description
-      const pDesc=Array.from(descElement.querySelectorAll('p'))!
-      const specsPossibleNames=['ESPECIFICAÇÕES','ESPECIFICAÇÃO','CARACTERÍSTICAS','CARACTERISTICAS','CARACTERISTICA']
-      const indexSpec=pDesc.indexOf(pDesc.find(element=>specsPossibleNames.some(string=>element.innerText.toUpperCase().includes(string)))!)
-      const specs=pDesc.slice(indexSpec,pDesc.length)
-      specs.forEach((p,index)=>{
-        index !== 0 && p.previousSibling?.remove()
-        p.remove()
-      })
-      const specsText = specs.map((e, index)=>{
-          if (e.innerHTML.includes('\u003Cbr\u003E')) {
-              const arry = e.innerHTML.split('\u003Cbr\u003E')
-              arry.shift()
-              return arry
-          } else {
-              if (e.innerText.includes(':')) {
-                  if (index !== 0) {
-                      return e.innerText
-                  }
-              }
-          }
-
-          return ''
-      }).filter(e=>e!=='' && e)
-
-      const specsObj:string[] & Array<string[]> = []
-      specsText.forEach((element)=>{
-          if (typeof (element) !== 'string') {
-              element!.forEach((arr)=>{
-                  let[key,value]:string[] = arr.split(':')
-
-                  if (!((key === undefined || value === undefined) ||  (/^\s*$/.test(key) || /^\s*$/.test(value)) || !(/[A-Za-z]/.test(key) || /[A-Za-z]/.test(value)) || (key.includes('\u003C') || value.includes('\u003C')))) {
-                    if (key.split('')[0] === '-') {
-                          const arrKey = key.split('')
-                          arrKey.shift()
-                          key = arrKey.join('')
-                      }
-                      specsObj.push([key, value])
-                  }
-              })
-          } else {
-              let[key,value] = element.split(':')
-
-              if (!((key === undefined || value === undefined) || (/^\s*$/.test(key) || /^\s*$/.test(value)) || !(/[A-Za-z]/.test(key) || /[A-Za-z]/.test(value)) || (key.includes('\u003C') || value.includes('\u003C')))) {
-                  if (key.split('')[0] === '-') {
-                      const arrKey = key.split('')
-                      arrKey.shift()
-                      key = arrKey.join('')
-                  }
-                  specsObj.push([key, value])
-              }
-          }
-      })
-
-      const table1 = document.createElement('table')
-      table1.classList.add('w-[90%]','re1:w-[45%]','text-left')
-      table1.innerHTML = '<tbody></tbody>'
-      const tbody1 = table1.querySelector('tbody')!
-
-      const table2 = document.createElement('table')
-      table2.classList.add('w-[90%]','re1:w-[45%]','text-left')
-      table2.innerHTML = '<tbody></tbody>'
-      const tbody2 = table2.querySelector('tbody')!
-
-      const middleIndex = Math.ceil((specsObj.length - 1) / 2)
-      const isOdd = specsObj.length % 2 === 0
-
-      specsObj.forEach(function(e, index) {
-        const [key,value] = e
-        const tr = document.createElement('tr')
-        if (((index + 1) % 2) !== 0) {
-          tr.classList.add('bg-[#151515]','re1:bg-transparent')
-        }
-        tr.innerHTML = `
-          <th class="w-[35%] text-neutral-content pl-4 text-normal">${key}</th><td class="w-[65%] pl-4 text-normal">${value}</td>
-        `
-        tr.classList.add('re1:even:bg-[#151515]','text-sm','h-[40px]')
-        if (isOdd) {
-            index > (specsObj.length / 2) - 1 ? tbody2.append(tr) : tbody1.append(tr)
-        } else {
-            index > middleIndex ? tbody2.append(tr) : tbody1.append(tr)
-        }
-      
-      })
-
-      const tableWrapper = document.createElement('div')
-      tableWrapper.classList.add('flex','flex-col','re1:flex-row','re1:gap-5','justify-center','items-center','re1:items-baseline')
-      tableWrapper.append(table1)
-      tableWrapper.append(table2)
-      
-      tableWrapper.innerText !== '' ? (productSpec.current && productSpec.current.append(tableWrapper)) : setSpecVazio(true)
-    }
-    
-    return false
-  })
+  const [openMenu,setOpenMenu]=useState(false)
 
   const [specVazio,setSpecVazio]=useState(false)
   const productSpec=useRef<HTMLDivElement>(null)
 
+  const [alreadyOpened,setAlreadyOpened]=useState(false)
+
   const handleDropdown=(event:MouseEvent)=>{
+    try{
+      if(!alreadyOpened && !PCGamer){
+        const descElement=document.createElement('div')
+        descElement.innerHTML=description
+        const pDesc=Array.from(descElement.querySelectorAll('p'))!
+        const specsPossibleNames=['ESPECIFICAÇÕES','ESPECIFICAÇÃO','CARACTERÍSTICAS','CARACTERISTICAS','CARACTERISTICA']
+        const indexSpec=pDesc.indexOf(pDesc.find(element=>specsPossibleNames.some(string=>element.innerText.toUpperCase().includes(string)))!)
+        const specs=pDesc.slice(indexSpec,pDesc.length)
+        specs.forEach((p,index)=>{
+          index !== 0 && p.previousSibling?.remove()
+          p.remove()
+        })
+        const specsText = specs.map((e, index)=>{
+            if (e.innerHTML.includes('\u003Cbr\u003E')) {
+                const arry = e.innerHTML.split('\u003Cbr\u003E')
+                arry.shift()
+                return arry
+            } else {
+                if (e.innerText.includes(':')) {
+                    if (index !== 0) {
+                        return e.innerText
+                    }
+                }
+            }
+  
+            return ''
+        }).filter(e=>e!=='' && e)
+  
+        const specsObj:string[] & Array<string[]> = []
+        specsText.forEach((element)=>{
+            if (typeof (element) !== 'string') {
+                element!.forEach((arr)=>{
+                    let[key,value]:string[] = arr.split(':')
+  
+                    if (!((key === undefined || value === undefined) ||  (/^\s*$/.test(key) || /^\s*$/.test(value)) || !(/[A-Za-z]/.test(key) || /[A-Za-z]/.test(value)) || (key.includes('\u003C') || value.includes('\u003C')))) {
+                      if (key.split('')[0] === '-') {
+                            const arrKey = key.split('')
+                            arrKey.shift()
+                            key = arrKey.join('')
+                        }
+                        specsObj.push([key, value])
+                    }
+                })
+            } else {
+                let[key,value] = element.split(':')
+  
+                if (!((key === undefined || value === undefined) || (/^\s*$/.test(key) || /^\s*$/.test(value)) || !(/[A-Za-z]/.test(key) || /[A-Za-z]/.test(value)) || (key.includes('\u003C') || value.includes('\u003C')))) {
+                    if (key.split('')[0] === '-') {
+                        const arrKey = key.split('')
+                        arrKey.shift()
+                        key = arrKey.join('')
+                    }
+                    specsObj.push([key, value])
+                }
+            }
+        })
+  
+        const table1 = document.createElement('table')
+        table1.classList.add('w-[90%]','re1:w-[45%]','text-left')
+        table1.innerHTML = '<tbody></tbody>'
+        const tbody1 = table1.querySelector('tbody')!
+  
+        const table2 = document.createElement('table')
+        table2.classList.add('w-[90%]','re1:w-[45%]','text-left')
+        table2.innerHTML = '<tbody></tbody>'
+        const tbody2 = table2.querySelector('tbody')!
+  
+        const middleIndex = Math.ceil((specsObj.length - 1) / 2)
+        const isOdd = specsObj.length % 2 === 0
+  
+        specsObj.forEach(function(e, index) {
+          const [key,value] = e
+          const tr = document.createElement('tr')
+          if (((index + 1) % 2) !== 0) {
+            tr.classList.add('bg-[#151515]','re1:bg-transparent')
+          }
+          tr.innerHTML = `
+            <th class="w-[35%] text-neutral-content pl-4 text-normal">${key}</th><td class="w-[65%] pl-4 text-normal">${value}</td>
+          `
+          tr.classList.add('re1:even:bg-[#151515]','text-sm','h-[40px]')
+          if (isOdd) {
+              index > (specsObj.length / 2) - 1 ? tbody2.append(tr) : tbody1.append(tr)
+          } else {
+              index > middleIndex ? tbody2.append(tr) : tbody1.append(tr)
+          }
+        
+        })
+  
+        const tableWrapper = document.createElement('div')
+        tableWrapper.classList.add('flex','flex-col','re1:flex-row','re1:gap-5','justify-center','items-center','re1:items-baseline')
+        tableWrapper.append(table1)
+        tableWrapper.append(table2)
+        
+        tableWrapper.innerText !== '' ? (productSpec.current && productSpec.current.append(tableWrapper)) : setSpecVazio(true)
+      }
+    }catch(err){
+      console.error(err)
+    }
+    setAlreadyOpened(true)
     setOpenMenu(!openMenu)
   }
 
