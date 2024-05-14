a4575d56671365a811bd7dbd7471837c7dd267a5
Kauan Fiolo da Silva <102124569+kauanfiolo-04@users.noreply.github.com>
2024-05-14 10:34:12 -0300

    Update LiveSquad.tsx

--- sections/LPs/LiveSquad/LiveSquad.tsx
+++ sections/LPs/LiveSquad/LiveSquad.tsx
@@ -32,13 +32,13 @@ const LiveSquad=()=>{
       </div>
       <div className='bg-[url(https://shopinfo.vteximg.com.br/arquivos/LP-Mobile-da-live-Maio_04.jpg)] re1:bg-[url(https://shopinfo.vteximg.com.br/arquivos/LPdesktop-da-live-Maio_02.jpg)] bg-center bg-cover w-full h-max py-4'>
         <div className='flex flex-col justify-center items-center gap-3 mx-auto relative'>
-          <Image width={200} height={300} loading='lazy' src='https://shopinfo.vteximg.com.br/arquivos/Programação.png' className='w-[50%] re1:w-[30%] m-auto mb-[5%]'/>
-          <Image width={300} height={300} loading='lazy' src='https://shopinfo.vteximg.com.br/arquivos/Live 02-05.png' className='w-[50%] re1:w-[30%] m-auto mb-[5%]'/>
-          <Image width={300} height={300} loading='lazy' src='https://shopinfo.vteximg.com.br/arquivos/Live 09-05.png' className='w-[50%] re1:w-[30%] m-auto mb-[5%]'/>
-          <Image width={200} height={200} loading='lazy' src='https://shopinfo.vteximg.com.br/arquivos/Live 16-05.png' className='w-[70em] mr-[30%] absolute re1:mt-[10%] mt-[15%]'/>
-          <Image width={300} height={300} loading='lazy' src='https://shopinfo.vteximg.com.br/arquivos/Live 24-05.png' className='w-[50%] re1:w-[30%] m-auto mt-[10%] mb-[5%]'/>
-          <Image width={300} height={300} loading='lazy' src='https://shopinfo.vteximg.com.br/arquivos/Live 29-05.png' className='w-[50%] re1:w-[30%] m-auto mb-[5%]'/>
-          <Image width={200} height={300} loading='lazy' src='https://shopinfo.vteximg.com.br/arquivos/Apresentando Nádia.png' className='w-[50%] re1:w-[30%] m-auto'/>
+          <img width={200} height={300} loading='lazy' src='https://shopinfo.vteximg.com.br/arquivos/Programação.png' className='w-[50%] re1:w-[30%] m-auto mb-[5%]'/>
+          <img width={300} height={300} loading='lazy' src='https://shopinfo.vteximg.com.br/arquivos/Live 02-05.png' className='w-[50%] re1:w-[30%] m-auto mb-[5%]'/>
+          <img width={300} height={300} loading='lazy' src='https://shopinfo.vteximg.com.br/arquivos/Live 09-05.png' className='w-[50%] re1:w-[30%] m-auto mb-[5%]'/>
+          <img width={200} height={200} loading='lazy' src='https://shopinfo.vteximg.com.br/arquivos/Live 16-05.png' className='w-[70em] mr-[30%] absolute re1:mt-[10%] mt-[15%]'/>
+          <img width={300} height={300} loading='lazy' src='https://shopinfo.vteximg.com.br/arquivos/Live 24-05.png' className='w-[50%] re1:w-[30%] m-auto mt-[10%] mb-[5%]'/>
+          <img width={300} height={300} loading='lazy' src='https://shopinfo.vteximg.com.br/arquivos/Live 29-05.png' className='w-[50%] re1:w-[30%] m-auto mb-[5%]'/>
+          <img width={200} height={300} loading='lazy' src='https://shopinfo.vteximg.com.br/arquivos/Apresentando Nádia.png' className='w-[50%] re1:w-[30%] m-auto'/>
         </div>
       </div>
     </div>
